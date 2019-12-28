776a0a9c2a19a6e57e55f424a6964ea57d39b785
Jean-Baptiste Musso <jeanbaptiste@tcmlabs.fr>
2019-12-28 13:14:09 +0100

    Use Node.js 10 since 8 is about to reach EOL, by bumping nixpkgs to 19.09

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,11 +1,10 @@
-let rev = "19.03"; in
-
-let src = builtins.fetchTarball {
-  sha256 = "0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-}; in
-
-{
+let
+  rev = "19.09";
+  src = builtins.fetchTarball {
+    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  };
+in {
   inherit rev src;
   nixpkgs = import src;
 }
